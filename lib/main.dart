26437d7a7157e540422d90cd3fff1638af229b50
MathPrm <primotmathilde@gmail.com>
2025-06-10 17:21:11 +0200

    fix(security): remove keys from github

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:ur_games/auth/auth_gate.dart';
+import 'package:ur_games/env.dart';
 
 void main() async {
    WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: "https://qcujpgrywgkvjxxsrkjc.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjdWpwZ3J5d2drdmp4eHNya2pjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMDQ4MzgsImV4cCI6MjA2NDg4MDgzOH0.apRIfZat4n7ZtNVKjEGWs4t6Y9ALdSHHmpmRQrYoCkc",
+    url: URL,
+    anonKey: ANNON_KEY,
   );
   runApp(const MainApp());
 }
