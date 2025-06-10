b3d7ce6d4c6e9357b01e6ae1c8736a1f2b6fdecf
MathPrm <primotmathilde@gmail.com>
2025-06-10 17:27:52 +0200

    fix(debug): remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:ur_games/auth/auth_gate.dart';
-import 'package:ur_games/env.dart';
+import 'package:ur_games/secret/env.dart';
 
 void main() async {
    WidgetsFlutterBinding.ensureInitialized();
@@ -19,6 +19,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFF2A354C), // Couleur de fond en hexadécimal
       ),
